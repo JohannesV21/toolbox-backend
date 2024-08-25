c8a01be9a8fba97260ec3119ca078e810a935bfb
Johannes Villasana <johannes9966@gmail.com>
2024-08-25 19:32:27 -0400

    ⚡️ Implementacion de concurrencia para optimizacion de la consulta

--- src/Controllers/Files/FilesController.js
+++ src/Controllers/Files/FilesController.js
@@ -8,16 +8,16 @@ import { mapError } from "../../Utiils/Helpers/Errors/ErrorHelper.js";
 export const getFilesData = async (req, res) => {
   try {
     // res.status(200).json({ message: "Files Controller" });
-    const data = await getFilesListService();
+    // const data = await getFilesListService();
     // const data = await getFileService("test4.csv");
-    // const data = await fetchFilesData();
+    const data = await fetchFilesData();
     // console.log("Controller: ", data);
     res.status(200).send(data);
   } catch (error) {
     console.error(`Error fetching files data in controller: ${error.message}`);
     res.status(error.status || 500).json({
       error: error.type,
-      message: `${error.errorData.code} - ${error.message}`,
+      message: `${error.errorData?.code} - ${error.message}`,
     });
   }
 };
--- src/Services/Files/FilesServices.js
+++ src/Services/Files/FilesServices.js
@@ -1,30 +1,42 @@
 import { BACK_URL, http } from "../../Configs/HttpServices.js";
 import { mapError } from "../../Utiils/Helpers/Errors/ErrorHelper.js";
 import { formatFileData } from "../../Utiils/Helpers/Formatters/FormatLinesFiles.js";
+import pLimit from "p-limit";
+
+// Establece un límite de concurrencia
+const limit = pLimit(10);
 
 export const fetchFilesData = async () => {
   const filesList = await getFilesListService();
-  const data = [];
 
-  for (const fileName of filesList) {
-    try {
-      console.log("FileName: ", fileName);
-      const fileData = await getFileService(fileName);
-      console.log("FileData: ", fileData);
-      const formattedData = formatFileData(fileData);
-      if (formattedData.length > 0) {
-        data.push({ file: fileName, lines: formattedData });
-      }
-    } catch (error) {
-      const mappedError = mapError(error);
-      console.error(`Error fetching data for file: ${fileName}`, error);
-      throw new Error(mappedError.message);
-    }
+  try {
+    const dataPromises = filesList.map((fileName) =>
+      limit(async () => {
+        try {
+          console.log("FileName: ", fileName);
+          const fileData = await getFileService(fileName);
+          console.log("FileData: ", fileData);
+          const formattedData = formatFileData(fileData);
+          return { file: fileName, lines: formattedData };
+        } catch (error) {
+          console.error(`Error fetching data for file: ${fileName}`, error);
+          return null;
+        }
+      })
+    );
+
+    const results = await Promise.all(dataPromises);
+
+    // Filtra los resultados nulos y los archivos sin datos
+    const data = results.filter((result) => result && result.lines.length > 0);
+
+    console.log("Data fetching: ", data);
+
+    return data;
+  } catch (error) {
+    console.error("Error fetching files data: ", error);
+    throw error;
   }
-
-  console.log("Data fetching: ", data);
-
-  return data;
 };
 
 export const getFilesListService = async () => {
