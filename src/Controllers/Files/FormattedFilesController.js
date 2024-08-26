9a1540eea9863952ef3216a62307f33c8da00dbc
Johannes Villasana <johannes9966@gmail.com>
2024-08-25 20:25:12 -0400

    ✨ Datos de un archivo especifico por query params

--- src/Controllers/Files/FormattedFilesController.js
+++ src/Controllers/Files/FormattedFilesController.js
@@ -3,15 +3,19 @@ import {
   getFileService,
   getFilesListService,
 } from "../../Services/Files/FilesServices.js";
-import { mapError } from "../../Utiils/Helpers/Errors/ErrorHelper.js";
 
-export const getFilesData = async (req, res) => {
+export const FormattedFilesController = async (req, res) => {
   try {
-    // res.status(200).json({ message: "Files Controller" });
+    console.log(req.query.fileName);
+    const fileName = req.query.fileName;
+    let data = {};
+
+    if (fileName) {
+      data = await getFileService(fileName);
+    } else {
+      data = await fetchFilesData();
+    }
     // const data = await getFilesListService();
-    // const data = await getFileService("test4.csv");
-    const data = await fetchFilesData();
-    // console.log("Controller: ", data);
     res.status(200).send(data);
   } catch (error) {
     console.error(`Error fetching files data in controller: ${error.message}`);
